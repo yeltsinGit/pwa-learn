b7e3691b6820720b911a793348e74fcdfbf2ed5f
Leon Yalin <leon.yalin@cellebrite.com>
2019-09-23 19:32:26 +0300

    webpack: add plugins and LiveReload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import * as _ from 'lodash';
+import './style.css';
+import Icon from './icon.jpg';
 
-const div = document.createElement('div');
-div.innerText = 'Hello from Webpack';
-document.body.appendChild(div);
+document.addEventListener("DOMContentLoaded", () => {
+  const div = document.createElement('div');
+
+  div.innerText = 'Hello from Webpack3';
+  div.classList.add('hello');
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  div.appendChild(myIcon);
+
+  document.body.appendChild(div);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: ".",
+    port: 4000
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 };
